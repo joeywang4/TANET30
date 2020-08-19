954b14e211d5c49a0f4414c94b44bdee12c2d392
joeywang4 <wangjoey4@gmail.com>
2020-08-19 14:22:25 +0800

    Fix tab indent

--- backend/models/like.js
+++ backend/models/like.js
@@ -2,29 +2,29 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const LikeSchema = new Schema({
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	event: {
-		type: Schema.Types.ObjectId,
-		ref: 'Event',
-		required: true
-	},
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	state: {
-		type: Number,
-		required: true
-	},
-	timestamp: {
-		type: Number,
-		required: true
-	}
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: 'Event',
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  state: {
+    type: Number,
+    required: true
+  },
+  timestamp: {
+    type: Number,
+    required: true
+  }
 });
 
 // Creating a table within database with the defined schema
