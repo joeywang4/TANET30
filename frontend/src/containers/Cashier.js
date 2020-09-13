c8816b12c3662ae863a52a6e8b152bbfc6d5627d
yitingwu31 <animalfamily31@gmail.com>
2020-09-13 15:49:23 +0800

    Cashier Page

--- frontend/src/containers/Cashier.js
+++ frontend/src/containers/Cashier.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { Header, Icon, Divider } from 'semantic-ui-react';
-import { Payment } from './index';
+import React, { useState } from 'react';
+import { Header, Icon, Divider, Menu } from 'semantic-ui-react';
+import { Payment, History } from './index';
+
+const [SCAN, REC] = [0,1];
 
 export default () => {
+  const [activeItem, setActiveItem] = useState(SCAN);
+
+  let display = null;
+  switch (activeItem) {
+    default:
+    case SCAN:
+      display = <Payment />;
+      break;
+    case REC:
+      display = <History />;
+      break;
+  }
+
   return (
     <div style={{marginTop: "2em", width: "80%"}}>
       <Header as='h2' icon textAlign='center'>
         <Icon name='user circle' circular />
         <Header.Content>Cashier</Header.Content>
       </Header>  
-      <Divider horizontal>
-        <Header as='h4'>
-          <Icon name='dollar' />
-          Receive
-        </Header>
-      </Divider>
-      <Payment />
+      <Divider />
+      <Menu stackable widths = {2}>
+        <Menu.Item 
+          name = "Scan"
+          active = {activeItem === SCAN}
+          onClick = {_ => { setActiveItem(SCAN); }}
+        />
+        <Menu.Item 
+          name = "Record"
+          active = {activeItem === REC}
+          onClick = {_ => { setActiveItem(REC); }}
+        />
+      </Menu>
+      {display}
     </div>
   )
 }
