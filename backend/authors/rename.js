538ba227a74f92526b89c09852df66ece1efea23
andybi7676 <59821107+andybi7676@users.noreply.github.com>
2020-09-14 00:05:31 +0800

    Update rename.js
    Fixing rename.js. Now it can return failedAry with failure infos in it, and can show the status of the events being copied.

--- backend/authors/rename.js
+++ backend/authors/rename.js
@@ -1,41 +1,36 @@
-const express = require("express");
 const fs = require('fs');
 const Event = require("../models/event");
-const User = require("../models/user");
-const TX = require("../models/transaction");
-const Like = require("../models/like");
-const { listeners } = require("process");
-const { copy } = require("../routes/api");
-const { fail } = require("assert");
 
 const rename = async () => {
   const failedAry = [];
 
+  const solvedEvent = {};
+
   async function copyAndRename(orgPath, eventId, authorId) {
     const desPath = `./authors/filesInId/${eventId}`
-    console.log(`desPath: ${desPath}`);
-    fs.exists(desPath, (exists) => {
-      if(!exists) {
-        fs.mkdir(desPath, { recursive: false }, (err) => {
-          if (err) throw err;
+    return await fs.promises.mkdir(desPath, { recursive: false })
+      .then( () => {
+        fs.copyFile(orgPath, `${desPath}/${eventId}${authorId}.txt`, (err) => {
+          if(err) throw err;
+        })
+        return true;
+      })
+      .catch(err => {
+        if(err.errno === -4075) {
           fs.copyFile(orgPath, `${desPath}/${eventId}${authorId}.txt`, (err) => {
             if(err) throw err;
           })
-        });
-        return;
-      }
-      fs.copyFile(orgPath, `${desPath}/${eventId}${authorId}.txt`, (err) => {
-        if(err) throw err;
+          return true;
+        }
+        return false;
       })
-    });
-    
   }
 
   async function nameToId(path, event=null) {
     const dir = await fs.promises.opendir(path);
     for await (const dirent of dir) {
       if(dirent.isDirectory()){
-        Event.findOne({name: dirent.name})
+        await Event.findOne({name: dirent.name})
           .populate({
             path: 'author',
             select: '_id name email',
@@ -43,56 +38,54 @@ const rename = async () => {
               path: 'author'
             }
           })
-          .then(event => {
-            console.log(event._id);
-            nameToId(`${path}/${dirent.name}`, event);
+          .then(async event => {
+            await nameToId(`${path}/${dirent.name}`, event);
           })
           .catch(err => {
-            console.log({event: dirent.name, name: null, type: 'event not found'})
             failedAry.push({event: dirent.name, name: null, type: 'event not found'});
             console.log(err);
           });
       }
       else {
         if(!event) {
-          console.log({event: null, name: dirent.name, type: 'unsorted'});
           failedAry.push({event: null, name: dirent.name, type: 'unsorted'});
-          return;
         }
-        const authors = [...event.author];
-        authors.map( (author) => {
-          fs.promises.readFile(`${path}/${dirent.name}`)
-          .then((data) => {
-            const lines = data.toString().split('\n');
-            // console.log(lines, lines[0].slice(13).trim(), lines[1].slice(12).trim(), lines[2].slice(11).trim());
-            if (lines[0].slice(13).trim() === author.email) {
-              if(lines[1].slice(12).trim() !== author.name) {
-                console.log({event: event.name, name: dirent.name, type: 'wrong author name'});
-                failedAry.push({event: event.name, name: dirent.name, type: 'wrong author name'});
-                return;
+        else {
+          const authors = [...event.author];
+          const response = await Promise.all( authors.map( async (author) => {
+            try {
+              const data = await fs.promises.readFile(`${path}/${dirent.name}`);
+              const lines = data.toString().split('\n');
+              if (lines[0].slice(13).trim() === author.email) {
+                if(lines[1].slice(12).trim() !== author.name) {
+                  failedAry.push({event: event.name, name: dirent.name, type: 'wrong author name or email'});
+                  return 0;
+                }
+                if (lines[2].slice(11).trim() !== event.name) {
+                  failedAry.push({event: event.name, name: dirent.name, type: 'wrong event name'});
+                  return 0;
+                }
+                const status = await copyAndRename(`${path}/${dirent.name}`, event._id, author._id);
+                return status ? 1 : -1;
               }
-              if (lines[2].slice(11).trim() !== event.name) {
-                console.log({event: event.name, name: dirent.name, type: 'wrong event name'});
-                failedAry.push({event: event.name, name: dirent.name, type: 'wrong event name'});
-                return;
-              }
-              copyAndRename(`${path}/${dirent.name}`, event._id, author._id);
+              return null;
+            } catch (error) {
+              throw (error);
             }
-          })
-          .catch((err)=> {
-            console.log(err);
-          })
-        });
+          }) );
+          solvedEvent[event.name] = solvedEvent[event.name] ? solvedEvent[event.name].map( (val, idx) => (val===null ? response[idx] : val) ) : response;
+          // console.log(`${event.name}: ${response}`);
+        }
+        
       }
     }
-    return true;
   }
 
   await nameToId('./authors/filesInName')
   console.log(failedAry);
+  console.log(solvedEvent);
   return failedAry;
 }
-
 
 
 module.exports = rename;
